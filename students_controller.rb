7b6ed6ed4c4e0a6c2ce8e88e4c442e2022220161
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-08-21 16:22:35 +0100

    MVP Done

--- students_controller.rb
+++ students_controller.rb
@@ -24,13 +24,32 @@ get '/students/:id' do
   erb(:show)
 end
 
-
-
-
 # create
 
+post '/students' do
+  @students = Student.new(params)
+  @students.save()
+  erb(:create)
+end
+
 # edit
+get '/students/:id/edit' do
+  @students = Student.find(params[:id])
+  erb(:edit)
+end
 
 # update
 
+post '/students/:id' do
+  Student.new(params).update
+  redirect to '/students'
+end
+
+
 # destroy
+
+post '/students/:id/delete' do
+  student = Student.find(params[:id])
+  student.delete()
+  redirect to '/students'
+end
